3d4c0b3d4ea6be463d779cd82f72cb4f822127e2
shrey <shrey.dabhi23@gmail.com>
2024-03-29 12:19:44 +0530

    setup topic and schema for ml features

--- app/commons.py
+++ app/commons.py
@@ -3,3 +3,4 @@ class Config:
     BOOTSTRAP_SERVERS = "localhost:19092"
     SCHEMA_REGISTRY_URL = "http://localhost:18081"
     TOPIC_NAME_TRANSACTIONS = "transactions"
+    TOPIC_NAME_ML_FEATURES = "ml_features"
--- app/setup_kafka.py
+++ app/setup_kafka.py
@@ -3,6 +3,7 @@ from confluent_kafka.admin import AdminClient, NewTopic
 
 from commons import Config
 
+print("Setting up Kafka topics...")
 
 admin_client = AdminClient({"bootstrap.servers": Config.BOOTSTRAP_SERVERS, "client.id": "setup-kafka"})
 
@@ -13,7 +14,14 @@ transactions_topic = NewTopic(
     config={"retention.ms": 24 * 60 * 60 * 1000, "segment.ms": 24 * 60 * 60 * 1000},
 )
 
-created_topics = admin_client.create_topics(new_topics=[transactions_topic], validate_only=False)
+ml_features_topic = NewTopic(
+    topic=Config.TOPIC_NAME_ML_FEATURES,
+    num_partitions=4,
+    replication_factor=1,
+    config={"retention.ms": 24 * 60 * 60 * 1000, "segment.ms": 24 * 60 * 60 * 1000},
+)
+
+created_topics = admin_client.create_topics(new_topics=[transactions_topic, ml_features_topic], validate_only=False)
 
 for topic, future in created_topics.items():
     try:
@@ -22,15 +30,30 @@ for topic, future in created_topics.items():
     except Exception as e:
         print(f"Failed to create topic '{Config.TOPIC_NAME_TRANSACTIONS}': {e}")
 
-with open(f"app/avro/transaction.avsc") as f:
-    schema_str = f.read()
-
-record_schema = Schema(schema_str, "AVRO")
+print("Setting up schemas in schema registry...")
 
 schema_registry_client = SchemaRegistryClient({"url": Config.SCHEMA_REGISTRY_URL})
 
+print("Processing schema for transactions")
+
+with open(f"avro/transaction.avsc") as f:
+    ml_feature_schema_str = f.read()
+ml_feature_schema = Schema(ml_feature_schema_str, "AVRO")
+
 try:
-    schema_id = schema_registry_client.register_schema(Config.TOPIC_NAME_TRANSACTIONS, record_schema)
-    print(f"Schema for transactions registered successfully with schema id: {schema_id}")
+    schema_id = schema_registry_client.register_schema(Config.TOPIC_NAME_TRANSACTIONS, ml_feature_schema)
+    print(f"Schema registered successfully with schema id: {schema_id}")
+except Exception as e:
+    print(f"Schema registration failed with error: {e}")
+
+print("Processing schema for ML features")
+
+with open(f"avro/ml_feature.avsc") as f:
+    ml_feature_schema_str = f.read()
+ml_feature_schema = Schema(ml_feature_schema_str, "AVRO")
+
+try:
+    schema_id = schema_registry_client.register_schema(Config.TOPIC_NAME_ML_FEATURES, ml_feature_schema)
+    print(f"Schema registered successfully with schema id: {schema_id}")
 except Exception as e:
     print(f"Schema registration failed with error: {e}")
