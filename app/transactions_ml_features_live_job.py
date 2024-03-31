87889a7be1ac89f6ec03e085b9c4436ee6aa7ca4
shrey <shrey.dabhi23@gmail.com>
2024-04-01 04:08:24 +0530

    update job for live ml feature computation

--- app/transactions_ml_features_live_job.py
+++ app/transactions_ml_features_live_job.py
@@ -5,7 +5,7 @@ from commons import Config
 
 
 env = StreamExecutionEnvironment.get_execution_environment()
-env.set_parallelism(4)
+env.set_parallelism(2)
 
 tenv = StreamTableEnvironment.create(env)
 
@@ -37,30 +37,33 @@ tenv.execute_sql(create_kafka_table)
 
 create_live_kafka_table = f"""
     create table ml_features_live (
-        user_id INT NOT NULL,
-        total_transactions_count INT NOT NULL,
+        user_id STRING NOT NULL,
+        total_transactions_count STRING NOT NULL,
         PRIMARY KEY (user_id) NOT ENFORCED
     ) with (
-        'connector' = 'kafka-upsert',
+        'connector' = 'upsert-kafka',
         'topic' = '{Config.TOPIC_NAME_ML_FEATURES_LIVE}',
         'properties.bootstrap.servers' = '{Config.BOOTSTRAP_SERVERS_DOCKER}',
         'properties.group.id' = 'transactions-ml-features-job-user',
         'key.format' = 'csv',
-        'key.fields' = 'user_id',
-        'value.format' = 'avro-confluent',
-        'value.avro-confluent.url' = '{Config.SCHEMA_REGISTRY_URL_DOCKER}',
-        'value.avro-confluent.subject' = '{Config.SCHEMA_SUBJECT_ML_FEATURES_LIVE}'
+        'key.fields-prefix' = 'user_',
+        'value.format' = 'csv',
+        'value.fields-include'='EXCEPT_KEY'
     );
 """
+# 'value.avro-confluent.url' = '{Config.SCHEMA_REGISTRY_URL_DOCKER}',
+#         'value.avro-confluent.subject' = '{Config.SCHEMA_SUBJECT_ML_FEATURES_LIVE}',
 
 tenv.execute_sql(create_live_kafka_table)
 
 agg_query_live = """
     INSERT INTO ml_features_live
     SELECT
-        user_id,
-        CAST(COUNT(user_id) AS INT)
-    FROM kafka_transactions;
+        CAST(user_id AS STRING),
+        CAST(CAST(COUNT(user_id) AS INT) AS STRING)
+    FROM kafka_transactions
+    GROUP BY user_id
+    HAVING CAST(CAST(COUNT(user_id) AS INT) AS STRING) IS NOT NULL;
 """
 
 res_live = tenv.execute_sql(agg_query_live)
